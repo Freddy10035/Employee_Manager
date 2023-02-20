a9f19ff3e78545db0d0212fed6682115343794c0
Fredrick Ochieng <fredrick_ochieng@outlook.com>
2023-02-20 16:30:55 +0300

    DOB and title of courtesy added.✅✅

--- Employee_Manager.Module/BusinessObjects/Employee.cs
+++ Employee_Manager.Module/BusinessObjects/Employee.cs
@@ -1,77 +1,4 @@
-﻿/*using DevExpress.Data.Filtering;
-using DevExpress.ExpressApp;
-using DevExpress.ExpressApp.DC;
-using DevExpress.ExpressApp.Model;
-using DevExpress.ExpressApp.Xpo;
-using DevExpress.Persistent.Base;
-using DevExpress.Persistent.BaseImpl;
-using DevExpress.Persistent.Validation;
-using DevExpress.Xpo;
-using DevExpress.Xpo.DB;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-
-namespace Employee_Manager.Module.BusinessObjects
-{
-    [DefaultClassOptions]
-    [Persistent("Employees")]
-    public class Employee : BaseObject
-    {
-        public Employee(Session session)
-            : base(session) { }
-
-        public override void AfterConstruction()
-        {
-            base.AfterConstruction();
-            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
-        }
-
-        string middleName;
-        string lastName;
-        string firstName;
-
-        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
-        public string FirstName
-        {
-            get => firstName;
-            set => SetPropertyValue(nameof(FirstName), ref firstName, value);
-        }
-
-
-        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
-        public string LastName
-        {
-            get => lastName;
-            set => SetPropertyValue(nameof(LastName), ref lastName, value);
-        }
-
-        
-        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
-        public string MiddleName
-        {
-            get => middleName;
-            set => SetPropertyValue(nameof(MiddleName), ref middleName, value);
-        }
-
-
-        public class Employee_ManagerXpoDbContext : XPObjectSpaceProvider
-        {
-            public Employee_ManagerXpoDbContext(IDataStore dataStoreProvider) : base(dataStoreProvider) { }
-
-            public XPCollection<Employee> Employees
-            {
-                get { return new XPCollection<Employee>(this); }
-            }
-        }
-
-    }
-}*/
-
-
-using DevExpress.Data.Filtering;
+﻿using DevExpress.Data.Filtering;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.DC;
 using DevExpress.ExpressApp.Kpi;
@@ -87,11 +14,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Employee_Manager.Module.BusinessObjects
 {
     [DefaultClassOptions]
-    [Persistent("Employees")]
     public class Employee : BaseObject
     {
         public Employee(Session session)
@@ -103,6 +30,7 @@ namespace Employee_Manager.Module.BusinessObjects
             // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
         }
 
+        DateTime birthday;
         string middleName;
         string lastName;
         string firstName;
@@ -130,10 +58,26 @@ namespace Employee_Manager.Module.BusinessObjects
             set => SetPropertyValue(nameof(MiddleName), ref middleName, value);
         }
 
+        
+        public DateTime Birthday
+        {
+            get => birthday;
+            set => SetPropertyValue(nameof(Birthday), ref birthday, value);
+        }
+
+
+        [Browsable(false)]
+        public int TitleOfCourtesy_Int { get; set; }
+
+        [NotMapped]
+        public TitleOfCourtesy TitleOfCourtesy { get; set; }
+
+
 
         public class EmployeeManagerXpoDataStoreProvider : IXpoDataStoreProvider
         {
-            private readonly IDataStore dataStore;
+            DateTime birthday;
+        private readonly IDataStore dataStore;
 
             public EmployeeManagerXpoDataStoreProvider(IDataStore dataStore)
             {
@@ -173,6 +117,15 @@ namespace Employee_Manager.Module.BusinessObjects
                 throw new NotImplementedException();
             }
         }
+    }
+
+    public enum TitleOfCourtesy
+    {
+        Dr,
+        Miss,
+        Mr,
+        Mrs,
+        Ms
     }
 }
 
