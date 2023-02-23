605f9fe96fe252d96477106f0d9c39dcee78cfed
Fredrick Ochieng <fredrick_ochieng@outlook.com>
2023-02-23 14:37:18 +0300

    Notes linking to description on the the description tab on the Tasks fields using "show notes" ✅✅

---
+++ Employee_Manager.Module/BusinessObjects/Note.cs
@@ -0,0 +1,46 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Employee_Manager.Module.BusinessObjects;
+
+
+[DefaultProperty(nameof(Text))]
+[ImageName("BO_Note")]
+
+public class Note : BaseObject
+{ 
+    public Note(Session session)
+        : base(session)
+    {
+    }
+
+    [DevExpress.ExpressApp.Data.Key, Browsable(false)]
+    [VisibleInDetailView(false), VisibleInListView(false), VisibleInLookupListView(false)]
+    public Guid ID { get; set; }
+    public String Author { get; set; }
+    public DateTime? DateTime { get; set; }
+
+    [FieldSize(FieldSizeAttribute.Unlimited)]
+    public String Text { get; set; }
+
+
+
+    public override void AfterConstruction()
+    {
+        base.AfterConstruction();
+        // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+    }
+   
+}
