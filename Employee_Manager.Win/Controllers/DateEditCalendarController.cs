66fc54d54bb74bff79189c5c5022c0e4c5dcedcc
Fredrick Ochieng <fredrick_ochieng@outlook.com>
2023-02-27 15:53:48 +0300

    resharper.

--- Employee_Manager.Win/Controllers/DateEditCalendarController.cs
+++ Employee_Manager.Win/Controllers/DateEditCalendarController.cs
@@ -1,22 +1,7 @@
-﻿using DevExpress.Data.Filtering;
-using DevExpress.ExpressApp;
-using DevExpress.ExpressApp.Actions;
-using DevExpress.ExpressApp.Editors;
-using DevExpress.ExpressApp.Layout;
-using DevExpress.ExpressApp;
+﻿using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Editors;
 using DevExpress.XtraEditors;
-using DevExpress.ExpressApp.Model.NodeGenerators;
-using DevExpress.ExpressApp.SystemModule;
-using DevExpress.ExpressApp.Templates;
-using DevExpress.ExpressApp.Utils;
-using DevExpress.Persistent.Base;
-using DevExpress.Persistent.Validation;
 using Employee_Manager.Module.BusinessObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Employee_Manager.Win.Controllers
 {
