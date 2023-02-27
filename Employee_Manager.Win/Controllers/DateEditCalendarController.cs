95c5ebe31b512705d4d966883e86ebf51e2e7d39
Fredrick Ochieng <fredrick_ochieng@outlook.com>
2023-02-27 12:37:15 +0300

    Access the Settings of a Property Editor in a Detail View. The Birthday editor shows a scrollable date picker in its drop-down window:

---
+++ Employee_Manager.Win/Controllers/DateEditCalendarController.cs
@@ -0,0 +1,59 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.XtraEditors;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using Employee_Manager.Module.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Employee_Manager.Win.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class DateEditCalendarController : ObjectViewController<DetailView, Employee>
+    {
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public DateEditCalendarController()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+            View.CustomizeViewItemControl(this, SetCalendarView, nameof(Employee.Birthday));
+        }
+
+        private void SetCalendarView(ViewItem viewItem)
+        {
+            // Obtain the Component Adapter
+            DateEdit dateEdit = (DateEdit)viewItem.Control;
+
+            // Set the date picker display mode to scroll picker
+            dateEdit.Properties.CalendarView = DevExpress.XtraEditors.Repository.CalendarView.TouchUI;
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
